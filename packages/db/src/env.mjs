b62584245e96e1864ef93be53cb3a02ab1e72ec1
kitravee <kitravee.swk@gmail.com>
2024-08-16 00:37:00 +0700

    feat: init user database

---
+++ packages/db/src/env.mjs
@@ -0,0 +1,12 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DRIZZLE_DATABASE_URL: z.string().min(1),
+  },
+  runtimeEnv: {
+    DRIZZLE_DATABASE_URL: process.env.DRIZZLE_DATABASE_URL,
+  },
+  skipValidation: true,
+});
---
+++ packages/db/src/schema/users/users.ts
@@ -0,0 +1,67 @@
+import { relations } from "drizzle-orm";
+import {
+  serial,
+  text,
+  timestamp,
+  integer,
+  pgTable,
+  primaryKey,
+  boolean,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("user", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text("name"),
+  email: text("email").unique(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+  // createdAt: timestamp("created_at").notNull().defaultNow(),
+  // updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+// NEXT AUTH TABLES
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+});
+
+export const verificationTokens = pgTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (verificationToken) => ({
+    compositePk: primaryKey({
+      columns: [verificationToken.identifier, verificationToken.token],
+    }),
+  })
+);
+
+export const authenticators = pgTable(
+  "authenticator",
+  {
+    credentialID: text("credentialID").notNull().unique(),
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    providerAccountId: text("providerAccountId").notNull(),
+    credentialPublicKey: text("credentialPublicKey").notNull(),
+    counter: integer("counter").notNull(),
+    credentialDeviceType: text("credentialDeviceType").notNull(),
+    credentialBackedUp: boolean("credentialBackedUp").notNull(),
+    transports: text("transports"),
+  },
+  (authenticator) => ({
+    compositePK: primaryKey({
+      columns: [authenticator.userId, authenticator.credentialID],
+    }),
+  })
+);
