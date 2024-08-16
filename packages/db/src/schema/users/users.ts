55f7cf93b76cf073dbed23f62fd883527530861a
kitravee <kitravee.swk@gmail.com>
2024-08-17 00:36:23 +0700

    fix: add account schema

--- packages/db/src/schema/users/users.ts
+++ packages/db/src/schema/users/users.ts
@@ -8,6 +8,7 @@ import {
   primaryKey,
   boolean,
 } from "drizzle-orm/pg-core";
+import type { AdapterAccount } from "next-auth/adapters";
 
 export const users = pgTable("user", {
   id: text("id")
@@ -22,6 +23,29 @@ export const users = pgTable("user", {
 });
 
 // NEXT AUTH TABLES
+export const accounts = pgTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount["type"]>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
+);
 
 export const sessions = pgTable("session", {
   sessionToken: text("sessionToken").primaryKey(),
