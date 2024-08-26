b2d4a0c96ac13b0d20494c0430a293cb577a1f31
kitravee <kitravee.swk@gmail.com>
2024-08-26 22:34:35 +0700

    fix: web page content

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -12,14 +12,13 @@ export default function Home() {
         <section id="hero" className="container">
           <div className="flex flex-col gap-6 md:gap-8 justify-center align-middle pt-5 md:pt-28">
             <h1 className="font-sans text-black dark:text-white relative mx-0 max-w-[43.5rem] pt-5 md:mx-auto md:px-4 md:py-2 text-left tracking-tighter text-balance md:text-center font-semibold md:text-7xl lg:text-7xl sm:text-7xl text-5xl">
-              Unify Your Digital Ecosystem
+              Integrations made easy
             </h1>
             <div className="flex md:justify-center">
               <div className="max-w-xl text-left md:text-center">
                 <p className="font-mono text-balance text-base tracking-tight text-black/75 dark:font-medium dark:text-white/75 md:text-lg ">
-                  streamlines your business by unifying digital tools and
-                  processes into a seamless, easy-to-manage platform, empowering
-                  you to optimize operations and drive growth effortlessly.
+                  Spend less time integrating and more time growing your
+                  business. Let us handle the integration for you.
                 </p>
               </div>
             </div>
