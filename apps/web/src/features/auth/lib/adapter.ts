9b86102fdfdf27e57bebe6bc682b506ff70b4f5b
kitravee <kitravee.swk@gmail.com>
2024-08-27 00:10:15 +0700

    fix: force type

--- apps/web/src/features/auth/lib/adapter.ts
+++ apps/web/src/features/auth/lib/adapter.ts
@@ -2,9 +2,10 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db, users, accounts, sessions, verificationTokens } from "@repo/db";
 import { Adapter } from "next-auth/adapters";
 import { createUser, getUser } from "./action";
+import { SqlFlavorOptions } from "@auth/drizzle-adapter/lib/utils";
 
 export const adapter: Adapter = {
-  ...DrizzleAdapter(db, {
+  ...DrizzleAdapter(db as SqlFlavorOptions, {
     usersTable: users,
     accountsTable: accounts,
     sessionsTable: sessions,
