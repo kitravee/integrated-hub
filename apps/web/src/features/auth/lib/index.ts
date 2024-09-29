53658b7c6da6dd931ae50949987f767bf8c1ce21
kitravee <kitravee.swk@gmail.com>
2024-09-29 14:21:03 +0700

    feat: Add route for dashboard page

--- apps/web/src/features/auth/lib/index.ts
+++ apps/web/src/features/auth/lib/index.ts
@@ -1,8 +1,9 @@
-import { adapter } from "./adapter";
 import { db, eq, users } from "@repo/db";
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
 import google from "next-auth/providers/google";
+
+import { adapter } from "./adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   debug: process.env.NODE_ENV === "development",
